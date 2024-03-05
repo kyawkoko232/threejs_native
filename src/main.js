05eea44db54afb5de8e522bcee7905f54175f847
kyawkoko232 <kyawkoko9m@gmail.com>
2024-03-05 19:43:51 +0630

    start

--- src/main.js
+++ src/main.js
@@ -1,1 +1,94 @@
 import './style.css'
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+
+//Canvas
+const canvas = document.querySelector('canvas.webgl')
+
+//Scene
+const scene = new THREE.Scene();
+
+//Geometry 
+const geometry = new THREE.TorusKnotGeometry( 10, 3, 100, 16 ); 
+
+//Material
+const material = new THREE.MeshNormalMaterial();
+
+//Mesh 
+const torusKnot = new THREE.Mesh(geometry, material); 
+torusKnot.scale.set(0.2, 0.2, 0.2);
+
+scene.add(torusKnot);
+
+
+
+/**
+ * Sizes
+ */
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight
+}
+
+window.addEventListener('resize', () =>
+{
+    // Update sizes
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
+
+    // Update camera
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
+
+    // Update renderer
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
+
+/**
+ * Camera
+ */
+// Base camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+camera.position.x = 1
+camera.position.y = 1
+camera.position.z = 10
+scene.add(camera)
+
+// Controls
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+})
+renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+/**
+ * Animate
+ */
+const clock = new THREE.Clock()
+
+const tick = () =>
+{
+    const elapsedTime = clock.getElapsedTime()
+
+    // Rotate the torus knot
+    torusKnot.rotation.x = elapsedTime * 0.2; // Rotate around x-axis
+    torusKnot.rotation.y = elapsedTime * 0.4; // Rotate around y-axis
+
+    // Update controls
+    controls.update()
+
+    // Render
+    renderer.render(scene, camera)
+
+    // Call tick again on the next frame
+    window.requestAnimationFrame(tick)
+}
+
+tick()
