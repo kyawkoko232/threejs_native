805810d04900a8ce08e1ad62c0f2ace7710d705a
kyawkoko232 <kyawkoko9m@gmail.com>
2024-03-05 19:16:37 +0630

    start

---
+++ vite.config.js
@@ -0,0 +1,35 @@
+import { defineConfig } from 'vite';
+import { resolve } from 'path'
+import path from 'path';
+import glsl from 'vite-plugin-glsl';
+export default {
+    root: 'src/',
+    publicDir: '../static/',
+    base: './',
+    server: {
+        port: 8080,
+        hot: true,
+        host : true
+      },
+    build: {   
+        rollupOptions: {
+            input: {
+                home : '/index.html',         
+              },
+    
+        },
+        outDir: '../dist',
+        emptyOutDir: true,
+        sourcemap: true
+    },
+
+    resolve: {
+        alias: {
+          '@node_modules': path.resolve(__dirname, 'node_modules/'),
+          '@/' : path.resolve(__dirname, 'static/'),
+        },
+      },
+      plugins : [
+        glsl()
+      ]
+}
